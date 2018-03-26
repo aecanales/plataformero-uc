b262f70cfba51fc4e2ac2903699b9ea25af10af5
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-03-26 12:46:50 -0300

    Colectable general concept

---
+++ Assets/Scripts/colectables/Colectable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colectable : MonoBehaviour {
+
+    public string defaulTag = "palyer";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(defaulTag)) {
+            collision.gameObject.SendMessage("trigger", "test" );
+        }
+    }
+}
---
+++ Assets/Scripts/colectables/Moneda.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Moneda : Colectable
+{
+
+    public int Amount = 10;
+
+    public Moneda()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        base.Update();
+	}
+}
