6464001445d2ed7562c5b51a70c4f0084b1f8a0a
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-03-26 15:33:28 -0300

    add :moneybag:

--- Assets/Scripts/colectables/Colectable.cs
+++ Assets/Scripts/colectables/Colectable.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class Colectable : MonoBehaviour {
 
-    public string defaulTag = "palyer";
+    public string triggerBy = "Player";
+    public float maxRange = 100;
+    public float velocidad = 10;
+    private Vector2 yMovement = new Vector2(0, 0);
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    protected virtual void Start () {
+        yMovement.y += transform.position.y;
+        yMovement.x += transform.position.x;
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Update is called once per frame
+    protected virtual void Update() {
+        yMovement.y += maxRange / 2 * Mathf.Sin(velocidad*Time.time);
+        Debug.Log(yMovement);
+    }
+
+    protected virtual void FixedUpdate()
     {
-        if (collision.gameObject.CompareTag(defaulTag)) {
-            collision.gameObject.SendMessage("trigger", "test" );
+        transform.position = yMovement * Time.fixedDeltaTime;
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(triggerBy)) {
+            Destroy(gameObject);
         }
     }
 }
