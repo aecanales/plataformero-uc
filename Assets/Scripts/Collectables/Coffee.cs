fdb0c3f076e542db8344103a71ae92e2c51741fd
aecanales <aecanales@uc.cl>
2018-04-03 17:43:31 -0300

    :memo: Adding files with corrected names

---
+++ Assets/Scripts/Collectables/Coffee.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coffee : Collectable {
+
+    public int amount = 1;
+
+    public Coffee()
+    {
+
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+    protected override void Update()
+    {
+        base.Update();
+
+    }
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(triggerBy))
+        {
+            collision.gameObject.SendMessage("lifeUp", amount);
+        }
+        base.OnTriggerEnter2D(collision);
+    }
+}
---
+++ Assets/Scripts/Collectables/Notes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : Collectable {
+
+    public Notes()
+    {
+
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+    protected override void Update()
+    {
+        base.Update();
+
+    }
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(triggerBy))
+        {
+            // Do something
+        }
+        base.OnTriggerEnter2D(collision);
+    }
+}
