e84671bdb009a58c2ed11b2facd7a2241b422ac4
aecanales <aecanales@uc.cl>
2018-04-06 16:56:26 -0300

    Implemented not functional dog :dog:
    Activating the text object doesn't work yet, but @Zetaku will fix it

---
+++ Assets/Scripts/Doggo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doggo : MonoBehaviour {
+
+    public GameObject message;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Se activó onTriggerStay2D");
+
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Es el player :o");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("El jugador apretó F");
+                message.SetActive(true);
+            }
+        }
+    }
+}
