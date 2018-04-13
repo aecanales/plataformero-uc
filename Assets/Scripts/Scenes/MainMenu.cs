e8a6cb3b4a3f21b196c7d2b3cd2e60476c6bb72b
aecanales <aecanales@uc.cl>
2018-04-13 18:21:32 -0300

    :star: Added menu and New Game button

---
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string CutsceneScene;
+
+    void StartGame()
+    { SceneManager.LoadScene(CutsceneScene); }
+}
