076d49c8b68c5afaa0fbd5bf1b521cbb49e2007b
aecanales <aecanales@uc.cl>
2018-04-13 19:10:01 -0300

    :star: Added credits button to menu

--- Assets/Scripts/Scenes/MainMenu.cs
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
     public string CutsceneScene;
 
+    public GameObject StartGameButton;
+    public GameObject OpenCreditsButton;
+
+    public Text Credits;
+    public GameObject CloseCreditsButton;
+
     void StartGame()
     { SceneManager.LoadScene(CutsceneScene); }
+
+    void ShowCredits()
+    {
+        StartGameButton.SetActive(false);
+        OpenCreditsButton.SetActive(false);
+
+        Credits.gameObject.SetActive(true);
+        CloseCreditsButton.SetActive(true);
+    }
+
+    void HideCredits()
+    {
+        StartGameButton.SetActive(true);
+        OpenCreditsButton.SetActive(true);
+
+        Credits.gameObject.SetActive(false);
+        CloseCreditsButton.SetActive(false);
+    }
 }
