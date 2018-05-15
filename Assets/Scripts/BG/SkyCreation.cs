cfb0da4516fc94a08ba8d9b59d83a155d4a15b26
aecanales <aecanales@uc.cl>
2018-05-15 18:28:53 -0400

    :art: :page_facing_up: Added tiled sky background to test scene

---
+++ Assets/Scripts/BG/SkyCreation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Creates tiled sky backgrounds. Takes the game object as the origin and tiles them to the right.
+public class SkyCreation : MonoBehaviour {
+
+    public GameObject SkyPrefab;
+    public Vector3 StartingPosition;
+    public int NumberOfSkies;
+    public float TileWidth;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 1; i <= NumberOfSkies; i++)
+        {
+            Vector3 position = new Vector3(StartingPosition.x + TileWidth * i, StartingPosition.y, StartingPosition.z);
+            Instantiate(SkyPrefab, position, Quaternion.identity, gameObject.transform);
+        }
+		
+	}
+}
