a7bf8bf8d9fdc270caca99381f89ca11d23dee3c
aecanales <aecanales@uc.cl>
2018-07-17 16:51:00 -0400

    :sparkles: Added BatiCristo to level

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour {
 
     public string player_tag = "Player";
+
     private GameObject[] players;
     private GameObject player;
 
     public float freeMove = 1; //How much the player can move "freely".
+
+    public float cameraY; // The Y value the camera will stay at.
+    public float yWaitTime; // The amount of seconds the camera spend in the air before falling to cameraY position.
+    private bool hasWaited = false;
 
     // Use this for initialization
     void Start () {
@@ -19,6 +24,17 @@ public class PlayerCamera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         players = GameObject.FindGameObjectsWithTag(player_tag);
+
+        if (!hasWaited) {
+            yWaitTime -= Time.deltaTime;
+
+            if (yWaitTime <= 0)
+                hasWaited = true;
+        }
+        else {
+            if (transform.position.y > cameraY)
+                transform.position = new Vector3(transform.position.x, transform.position.y - 3 * Time.deltaTime, transform.position.z);
+        }
     }
 
     private void LateUpdate()
