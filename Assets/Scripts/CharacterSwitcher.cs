23bd51d9891695484cdd01f73ce1bf772c65bd9d
aecanales <aecanales@uc.cl>
2018-07-20 11:18:37 -0400

    :art: Added ability to switch between male and female animations

---
+++ Assets/Scripts/CharacterSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitcher : MonoBehaviour {
+
+    public RuntimeAnimatorController femaleAnimations;
+    public RuntimeAnimatorController maleAnimations;
+
+    private bool isFemale = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            if (isFemale)
+                GetComponent<Animator>().runtimeAnimatorController = maleAnimations;
+            else
+                GetComponent<Animator>().runtimeAnimatorController = femaleAnimations;
+            isFemale = !isFemale;
+        }
+		
+	}
+}
