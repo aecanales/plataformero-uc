25ee55434ebc2d16c54e37f3380664602434cd77
Alonso Canales <aecanales@uc.cl>
2018-08-31 20:15:04 -0300

    :movie_camera: Added movement and animations to teachers

--- Assets/Scripts/EvilTeacher.cs
+++ Assets/Scripts/EvilTeacher.cs
@@ -6,15 +6,32 @@ using Assets.Scripts.Collectables.Abilities;
 
 public class EvilTeacher : MonoBehaviour {
 
+    private Vector3 initialPos;
+    private float sine;
+
+    public float horizontalMovement;
+    public float speed;
+
 	// Use this for initialization
 	void Start () {
-		
+        initialPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Mirror();
+        Vector3 newPos = initialPos;
+        sine = Mathf.Sin(Time.time * speed);
+        newPos.x += horizontalMovement * sine;
+        transform.position = newPos;
 	}
+
+    void Mirror() {
+        if (sine - Mathf.Sin(Time.time * speed) > 0) // decreasing sine, going to left
+            transform.localScale = new Vector3(-0.5f, 0.5f, 1);
+        else // increasing sine, going to right
+            transform.localScale = new Vector3(0.5f, 0.5f, 1);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
